a98d5f9bdf80d161dfe106da00113889ad94ce96
João Pedro Theodoro <joaopedrotheodoroferreira@gmail.com>
2018-08-14 09:02:34 -0300

    Correção moedas

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -111,4 +111,9 @@ public class PlayerCtrl : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Coin")) {
+			Destroy(other.gameObject);
+		}
+	}
 }
